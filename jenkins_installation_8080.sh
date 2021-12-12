6d6ba8901bd48a13815bf7ac9ada74af5397cfe0
KaluvalaRamya <92627667+KaluvalaRamya@users.noreply.github.com>
2021-12-12 21:41:55 +0530

    Update jenkins_installation_8080.sh

--- jenkins_installation_8080.sh
+++ jenkins_installation_8080.sh
@@ -6,7 +6,7 @@ sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.li
 apt upgrade -y             #upgrade package
 apt-get -y update
 echo "Install jenkins"
-apt-get -y install jenkins         
+apt-get install jenkins=2.319         
 sleep "30"
 #echo "Change defualt port to 8082"
 #sed -i 's/8080/8082/' /etc/default/jenkins
@@ -14,7 +14,6 @@ echo "Allowing users to signup"
 sed -i 's/<disableSignup>true<\/disableSignup>/<disableSignup>false<\/disableSignup>/' /var/lib/jenkins/config.$
 echo "Restart jenkins server"
 systemctl restart jenkins
-apt-get install jenkins=2.319              #change the version of jenkins
 echo "Iniatial admin password"
 cat /var/lib/jenkins/secrets/initialAdminPassword
 echo "access your server on browser by <public-ip>:8080"
